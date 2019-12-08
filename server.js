0946f6e1a3f8acf055c16ac84c9a8cb170ae5d1b
Inspirefly <inspireflyy@gmail.com>
2019-12-08 14:49:46 -0600

    Server fild handling

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const path = require('path');
 
 const app = express();
 
-if(process.env.NODE_ENV === "production")
+if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname), 'client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use('/api/puzzles', require('./routes/api/puzzles'));
 
