12c41c746ca8b8ab88022a566fe79c18bfb7ac15
Inspirefly <inspireflyy@gmail.com>
2019-12-19 13:05:44 -0600

    Rerendering fewer cells

--- client/src/components/Cell.js
+++ client/src/components/Cell.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setLocation } from '../redux/actions/cellAction';
 import '../css/Cell.css';
-import PencilCell from './PencilCell';
+import CellNumber from './CellNumber';
+import PencilBox from './PencilBox';
 
 function Cell(props) {
   let origCell;
@@ -17,26 +18,26 @@ function Cell(props) {
     backgroundColor: 'white'
   }
 
-  if (JSON.stringify(props.location) === JSON.stringify(props.selectedCell)) {
+  if (props.isSelectedCell) {
     backgroundColor.backgroundColor = 'lightgray';
   }
 
   return (
     <div className="cell" onClick={setLocation} style={backgroundColor}>
-      <PencilCell origCell={origCell} location={props.location}/>
-      <div className="cell-number">{origCell ? <strong>{props.num}</strong> : props.num}</div>
+      <PencilBox origCell={origCell} location={props.location} />
+      <CellNumber origCell={origCell} location={props.location} />
     </div>
   );
 }
 
 const mapStateToProps = (state, ownProps) => {
-  let num = ' ';
-  if (ownProps.location && state.puzzles.user.length > 0)
-    num = state.puzzles.user[ownProps.location[0]][ownProps.location[1]];
+  let isSelectedCell = false; 
+  if (JSON.stringify(state.cells.selectedCell) === JSON.stringify(ownProps.location))
+    isSelectedCell = true;
   return ({
     unsolved: state.puzzles.unsolved,
-    selectedCell: state.cells.selectedCell,
-    num
+    isSelectedCell,
   });
 };
+
 export default connect(mapStateToProps, {setLocation})(Cell);
--- client/src/components/PencilCell.js
+++ client/src/components/PencilCell.js
@@ -1,33 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import uuidv4 from 'uuid/v4';
-import '../css/PencilCell.css';
 
 function PencilCell(props) {
-  const display = {
-    opacity: '0.75'
-  }
-
-  if (props.origCell) {
-    display.opacity = '0.0';
-  }
-
-  let cells = JSON.parse(props.cells);
-  const cellItems = cells.map(num => (
-    <div key={uuidv4()}>{num === ' ' ? '\xA0' : num}</div>
-  ));
-
   return (
-    <div className="pencil-cell" style={display}>
-      {cellItems}
+    <div>
+      {props.num === ' ' ? '\xA0' : props.num}
     </div>
   );
 }
 
 const mapStateToProps = (state, ownProps) => {
-  // Stringify because redux rerenders component based on shallow copy (===) which will always
-  // return false with new arrays, even if contents are the same, since the references aren't
-  let cells = JSON.stringify(state.cells.pencilCells[ownProps.location[0]][ownProps.location[1]]);
-  return({cells});
+  let num = ' ';
+  if (ownProps.location && state.cells.pencilCells)
+    num = state.cells.pencilCells[ownProps.location[0]][ownProps.location[1]][ownProps.location[2]];
+  return({num});
 };
+
 export default connect(mapStateToProps, null)(PencilCell);
